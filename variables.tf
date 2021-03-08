92fd2f47ed435507cca8ccf080a121137966b49c
Victor Rodrigues <viccomedy@gmail.com>
2021-03-08 16:09:35 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+    default = "us-east-1"
+    description = "Main Region"
+}
+
+variable "access_key" {
+    default = "xxxxxxxxxxxxxxx"
+}
+
+variable "secret_key" {
+    default = "xxxxxxxxxxxxxxxxxx"
+}
+
+variable "ami" {
+    default = "ami-0915bcb5fa77e4892"
+}
+
+variable "type" {
+    default = "t3.small"
+}
+
+variable "tags" {
+    default = {
+        "Name" = "datadog-lab"
+        "ScheduledStartStop" = "True"
+        "owner" = "Victor"
+        "env" = "<insert your env>"
+        
+    }
+}
